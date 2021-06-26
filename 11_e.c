3e70355f89ffa004dcf7d9e4c3989088a5eae641
Ricky <debojeetofficial@gmail.com>
2021-06-26 17:05:19 +0530

    :zap: Updated dfa structure

--- 11_e.c
+++ 11_e.c
@@ -1,116 +1,99 @@
-//C program to implement the transition diagram for relational operators
+/* C program to implement the transition diagram for relational operators */
 
 #include <stdio.h>
-#include <string.h>
 
-int dfa = 0;
-  
-void start(char c)
-{
-    if (c == '<') {
-        dfa = 1;
-    }
-    else if (c == '=') {
-        dfa = 5;
-    }
-    else if (c == '>') {
-        dfa = 6;
-    }
-    // -1 is used to check for any invalid symbol
-    else {
-        dfa = -1;
-    }
-}
-  
-// This function is for the first state (Q1) of DFA
-void state1(char c)
-{
-    if (c == '=') {
-        dfa = 2;
-    }
-    else if (c == '>') {
-        dfa = 3;
-    }
-    else {
-        dfa = 4;
-    }
-}
-  
-// This function is for the second state (Q2) of DFA
-void state6(char c)
-{
-    if (c == '=') {
-        dfa = 7;
-    }
-    else {
-        dfa = 8;
-    }
-}
-  
-  
-int isAccepted(char str[])
-{
-    // store length of string
-    int i, len = strlen(str);
-  
-    for (i = 0; i <= len; i++) {
-        if (dfa == 0)
-            start(str[i]);
-        else if (dfa == 1)
-            state1(str[i]);
-  
-        else if (dfa == 2){
-            printf("RELOP, LE");
-            return 2;
-        }
-            
-  
-        else if (dfa == 3) {
-            printf("RELOP, NE");
-            return 3;
-        }
-            
-  
-        else if (dfa == 4) {
-            printf("RELOP, LT");
-            return 4;
-        }
+/* Use a DFA approach.  state indicates the state of the DFA. */
+int state = 0;
 
-        else if (dfa == 5) {
-            printf("RELOP, EQ");
-            return 5;
-        }
+int start(int);
+int state1(int);
+int state6(int);
 
-        else if (dfa == 6) {
-            state6(str[i]);
-            return 6;
-        }
 
-        else if (dfa == 7) {
-            printf("RELOP, GE");
-            return 7;
-        }
-        
-            
-        else if (dfa == 8) {
-            printf("RELOP, GT");
-            return 8;
-        }
-
-        else {
-            printf("ERROR");
-            return -1;
-        }
-
-    }
-}
-  
-// driver code
+/*------------------------------------------------------------*/
+/* main: Read text from stdin. Check for the relop and return */  
+/* the result to stdout. Return 0                             */
+/*------------------------------------------------------------*/ 
 int main()
 {
-    char str[100];
-    printf("Input:\t");
-    scanf("%s", str);
-    isAccepted(str);
+    int c;
+    for( ; ; ) {
+        c = getchar();
+        if (c == EOF) 
+            break;
+        switch (state)
+        {
+            case 0:
+                state = start(c);
+                break;
+            case 1:
+                state = state1(c);
+                break;
+            case 2:
+                printf("RELOP, LE\n");
+                return 0;
+            case 3:
+                printf("RELOP, NE\n");
+                return 0;
+            case 4:
+                printf("RELOP, LT\n");
+                return 0;
+            case 5:
+                printf("RELOP, EQ\n");
+                return 0;
+            case 6:
+                state = state6(c);
+                break;
+            case 7:
+                printf("RELOP, GE\n");
+                return 0;
+            case 8:
+                printf("RELOP, GT\n");
+                return 0;
+        } 
+    }
     return 0;
+}
+
+/*------------------------------------------------------------*/
+/* start: Implement the state 0 of the DFA. c is the current  */
+/* DFA character.  Return the next state.                     */
+/*------------------------------------------------------------*/ 
+int start(int c)
+{
+    if (c == '<')
+        state = 1;
+    else if (c == '=')
+        state = 5;
+    else if (c == '>')
+        state = 6;
+    return state;
+}
+
+/*------------------------------------------------------------*/
+/* state1: Implement the state 1 of the DFA. c is the current */
+/* DFA character.  Return the next state.                     */
+/*------------------------------------------------------------*/
+int state1(int c)
+{
+    if (c == '=')
+        state = 2;
+    else if (c == '>')
+        state = 3;
+    else
+        state = 4;
+    return state;
+}
+
+/*------------------------------------------------------------*/
+/* state6: Implement the state 6 of the DFA. c is the current */
+/* DFA character.  Return the next state.                     */
+/*------------------------------------------------------------*/
+int state6(int c)
+{
+    if (c == '=')
+        state = 7;
+    else
+        state = 8;
+    return state;
 }
